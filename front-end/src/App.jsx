667a00ef05ea79a661471038678a01c91000780f
leomreis <leo-mreis@hotmail.com>
2025-10-15 09:45:33 -0300

    a

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PaginaInicial from './pages/PaginaInicial';
-import Login from './components/Login'
+import Login from './pages/Login'
 function App() {
   return (
     <BrowserRouter>
