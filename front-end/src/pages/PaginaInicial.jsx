994afa8a34382229b9807ffb5f22cd902319e58d
leomreis <leo-mreis@hotmail.com>
2025-10-15 14:48:52 -0300

    Criação de TAB's, cliente, venda, dashboard ate o momento, fomularios em Clientes (cadastro), botao de salvar, NAVBAR sumindo e reaparecendo dependendo do local da pagina, scroll cima e scroll baixo

--- front-end/src/pages/PaginaInicial.jsx
+++ front-end/src/pages/PaginaInicial.jsx
@@ -2,7 +2,7 @@ import NavBar from "../components/NavBar";
 
 export default function PaginaInicial() {
   return (
-    <div className="bg-gray-50 min-h-screen">
+    <div className="bg-gray-50 min-h-screen pt-20 px-8">
       <NavBar />
     </div>
   );
