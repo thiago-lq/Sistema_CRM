0fb09e82388a221bcbfaad81bd45e2c30b0b097c
thiago-lq <thiagolq100@gmail.com>
2025-10-15 12:12:59 -0300

    Mudanças pra você ai

--- front-end/src/components/Vendas.jsx
+++ front-end/src/components/Vendas.jsx
@@ -1,4 +1,4 @@
-export default function SecaoVendas() {
+export default function Vendas() {
   return (
     <div className="p-10 m-6 bg-white rounded-xl shadow-md mt-10">
       <h2 className="text-2xl font-bold mb-4 text-gray-800">Vendas</h2>
