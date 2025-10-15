6d087c4bfcad146ba835647dd7b96bd93fe7e1eb
thiago-lq <thiagolq100@gmail.com>
2025-10-15 09:57:40 -0300

    Atualiza front-end de leo e back-end do Túlio

---
+++ front-end/src/components/Bar.jsx
@@ -0,0 +1,14 @@
+import empresa from "../assets/icone_empresa.png";
+
+export default function Bar(){
+  return(
+    <nav className="fixed top-0 left-0 right-0 bg-white shadow-md flex items-center justify-between px-5 py-0 z-50">
+      <div className="flex items-center space-x-3">
+        <button className="flex items-center text-gray-700 hover:text-indigo-600 transition duration-150 p-2">
+          <img src={empresa} alt="empresa" className="h-25 w-25" />
+          <span className="text-3xl font-bold text-black">SEU CRM</span>
+        </button>
+      </div>
+  </nav>
+        );
+}
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import talwindcss from '@talwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), talwindcss()],
+  plugins: [react(), tailwindcss()],
 })
