bce995c01d17a4fc684c920c00f036f9d29e9d44
thiago-lq <thiagolq100@gmail.com>
2025-10-15 15:45:57 -0300

    Atualiza front do leo

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
-export default function SecaoDashboards() {
+export default function Dashboards() {
   return (
-    <div className="p-10 m-6 bg-white rounded-xl shadow-md">
+    <div className="p-10 m-6 bg-white rounded-xl shadow-[0_1px_30px_rgba(0,0,0,0.15)] mt-20">
       <h2 className="text-2xl font-bold mb-4 text-gray-800">Dashboard</h2>
       <p>Gráficos e indicadores do sistema CRM.</p>
     </div>
--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -3,10 +3,14 @@ import cliente from "../assets/icone_clientes.png";
 import venda from "../assets/icone_venda.png";
 import dashboard from "../assets/icone_dashboard.png";
 import logout from "../assets/icone_logout.png";
+
 import { useNavigate } from "react-router-dom";
-import Inicio from "./Inicio";
 import { useState, useEffect, useRef } from "react";
+
+import Inicio from "./Inicio";
 import Vendas from "./Vendas";
+import Clientes from "./Clientes";
+import Dashboards from "./Dashboard";
 
 const LinkStyle =
   "flex flex-col items-center p-2 rounded-lg text-gray-700 hover:opacity-60 hover:cursor-pointer transition duration-150";
@@ -51,7 +55,7 @@ export default function NavBar() {
 
   return (
     <div>
-      <nav className={`fixed top-0 left-0 right-0 bg-white shadow-md flex items-center justify-around px-0 py-2 z-50
+      <nav className={`fixed top-0 left-0 right-0 bg-white shadow-md flex items-center justify-around px-0 py-4 z-50
          transitio-transform duration-300 ${showNavbar ? "translate-y-0" : "-translate-y-full"}`}>
         <div className="flex items-center space-x-3 mr-auto pl-10">
           <button className="flex items-center text-gray-700 hover:opacity-60 hover:cursor-pointer transition duration-150 p-0"
@@ -71,13 +75,15 @@ export default function NavBar() {
           </button>
 
           <button
+          onClick={() => setTab("clientes")}
             className={LinkStyle}
           >
-            <img src={cliente} alt="cliente" className="h-12 w-12" />
+            <img src={cliente} alt="clientes" className="h-12 w-12" />
             <span className="text-xs mt-1 font-medium">CLIENTES</span>
           </button>
 
           <button
+          onClick={() => setTab("dashboards")}
             className={LinkStyle}
           >
             <img src={dashboard} alt="dashboard" className="h-12 w-12" />
@@ -92,9 +98,9 @@ export default function NavBar() {
       </nav>
       <div className="mt-15">
         {tab === "inicio" && <Inicio />}
-      </div>
-      <div className="mt-15">
         {tab === "vendas" && <Vendas />}
+        {tab === "clientes" && <Clientes />}
+        {tab === "dashboards" && <Dashboards />}
       </div>
     </div>
   );
