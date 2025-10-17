cbb3ca9ceb20de2e5075ad9f1c8b97057f44bca7
thiago-lq <thiagolq100@gmail.com>
2025-10-17 13:32:26 -0300

    Criação das rotas básicas de clientes

--- back-end/database/migrations/2025_10_13_124035_create_clientes_table.php
+++ back-end/database/migrations/2025_10_13_124035_create_clientes_table.php
@@ -14,14 +14,35 @@ return new class extends Migration
             CPF_CLIENTE CHAR(11) NOT NULL UNIQUE,
             EMAIL VARCHAR(100) NOT NULL,
             NOME VARCHAR(100) NOT NULL,
-            CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            UPDATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+            DATA_NASCIMENTO DATE NOT NULL,
+            CREATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo',
+            UPDATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo' 
           ); 
+        ");
+
+        DB::statement("
+          CREATE OR REPLACE FUNCTION atualizar_updated_at()
+          RETURNS TRIGGER AS \$\$
+          BEGIN
+            NEW.UPDATED_AT = CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo';
+            RETURN NEW;
+          END;
+          \$\$ LANGUAGE plpgsql;
+        ");
+
+        DB::statement("
+          CREATE TRIGGER trigger_updated_at
+          BEFORE UPDATE ON CLIENTES
+          FOR EACH ROW
+          EXECUTE FUNCTION atualizar_updated_at();
         ");
     }
 
     public function down()
     {
+        DB::statement("DROP TRIGGER IF EXISTS trigger_updated_at ON CLIENTES;");
+        DB::statement("DROP FUNCTION IF EXISTS atualizar_updated_at;");
+
         DB::statement("DROP TABLE IF EXISTS CLIENTES;");
     }
 };
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/clientes', [ClienteController::class, 'index']);
+Route::get('/clientes/{id}', [ClienteController::class, 'show']);
+Route::post('/clientes', [ClienteController::class, 'store']);
+Route::put('/clientes/{id}', [ClienteController::class, 'update']);
+Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
