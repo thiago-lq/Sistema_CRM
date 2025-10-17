ad730427886042f67fdcf1443e3028bf34cc083b
thiago-lq <thiagolq100@gmail.com>
2025-10-17 13:57:55 -0300

    Mudanças na migration clientes

--- back-end/database/migrations/2025_10_13_124035_create_clientes_table.php
+++ back-end/database/migrations/2025_10_13_124035_create_clientes_table.php
@@ -15,16 +15,34 @@ return new class extends Migration
             EMAIL VARCHAR(100) NOT NULL,
             NOME VARCHAR(100) NOT NULL,
             DATA_NASCIMENTO DATE NOT NULL,
-            CREATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo',
-            UPDATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo' 
+            CREATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+            UPDATED_AT TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP 
           ); 
+        ");
+
+        DB::statement("
+          CREATE OR REPLACE FUNCTION set_created_at()
+          RETURNS TRIGGER AS \$\$
+          BEGIN
+            NEW.CREATED_AT := CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo';
+            NEW.UPDATED_AT := CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo';
+            RETURN NEW;
+          END;
+          \$\$ LANGUAGE plpgsql;
+        ");
+
+        DB::statement("
+          CREATE TRIGGER trigger_created_at
+          BEFORE INSERT ON CLIENTES
+          FOR EACH ROW
+          EXECUTE FUNCTION set_created_at();
         ");
 
         DB::statement("
           CREATE OR REPLACE FUNCTION atualizar_updated_at()
           RETURNS TRIGGER AS \$\$
           BEGIN
-            NEW.UPDATED_AT = CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo';
+            NEW.UPDATED_AT := CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo';
             RETURN NEW;
           END;
           \$\$ LANGUAGE plpgsql;
@@ -40,6 +58,8 @@ return new class extends Migration
 
     public function down()
     {
+        DB::statement("DROP TRIGGER IF EXISTS trigger_created_at ON CLIENTES;");
+        DB::statement("DROP FUNCTION IF EXISTS set_created_at;");
         DB::statement("DROP TRIGGER IF EXISTS trigger_updated_at ON CLIENTES;");
         DB::statement("DROP FUNCTION IF EXISTS atualizar_updated_at;");
 
