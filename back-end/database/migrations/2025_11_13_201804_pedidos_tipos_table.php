99b4b3135466ba27e77a81e4c05f461d393839c9
thiago-lq <thiagolq100@gmail.com>
2025-11-13 17:44:23 -0300

    Mudanças nas migrations

---
+++ back-end/database/migrations/2025_11_13_201804_pedidos_tipos_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        DB::statement("
+            CREATE TABLE PEDIDOS_TIPOS (
+            COD_PEDIDO_TIPO SERIAL PRIMARY KEY,
+            COD_PEDIDO INT NOT NULL,
+            NOME_TIPO VARCHAR(20) CHECK (PEDIDO_TIPO IN ('INSTALACAO', 'MANUTENCAO', 'PRODUTO')) NOT NULL,
+            FOREIGN KEY (COD_PEDIDO) REFERENCES PEDIDOS(COD_PEDIDO) ON DELETE CASCADE
+        ");
+    }
+
+    public function down()
+    {
+        DB::statement("DROP TABLE IF EXISTS PEDIDOS_TIPOS");
+    }
+};
