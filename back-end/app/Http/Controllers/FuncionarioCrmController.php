31e801deae8d9788c96a83178943d86763b5b374
thiago-lq <thiagolq100@gmail.com>
2025-11-26 00:42:59 -0300

    Save

--- back-end/app/Http/Controllers/FuncionarioCrmController.php
+++ back-end/app/Http/Controllers/FuncionarioCrmController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class Funcionario_CrmController extends Controller
+class FuncionarioCrmController extends Controller
 {
 
     // Controlador que busca os todos os funcionarios, ou busca um funcionario específico pelo nome
