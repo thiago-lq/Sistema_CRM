688728cc4a34ea520811c43d2a11ff57e4bede30
thiago-lq <thiagolq100@gmail.com>
2026-02-01 09:32:30 -0300

    Vamo ver se o deploy funciona

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { supabase } from "./supabase";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
