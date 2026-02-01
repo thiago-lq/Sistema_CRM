8808fb5ead8a634a2341b103f8a8ca81ee3dc292
thiago-lq <thiagolq100@gmail.com>
2026-02-01 14:35:29 -0300

    Teste

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -21,17 +21,8 @@ return [
 
     // Domínios específicos do seu projeto Vercel
     'allowed_origins' => [
+        'http://localhost:5173',
         'https://sistema-crm-ten.vercel.app',
-        'https://sistema-crm-thiagos-projects-7e80f3b8.vercel.app',
-        'https://sistema-crm-git-main-thiagos-projects-7e80f3b8.vercel.app',
-        'https://sistema-aoszdxoic-thiagos-projects-7e80f3b8.vercel.app',
-    ],
-
-    // Padrões regex para permitir QUALQUER subdomínio .vercel.app
-    'allowed_origins_patterns' => [
-        '/^https?:\/\/(.+\.)?vercel\.app$/',
-        '/^https?:\/\/localhost(:\d+)?$/',
-        '/^https?:\/\/127\.0\.0\.1(:\d+)?$/',
     ],
 
     'allowed_origins_patterns' => [],
